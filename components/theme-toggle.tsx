bf1def49b788f34e1e97906709adc212d385b1de
Rasmus Falk <1998.rasmus.falk@gmail.com>
2023-06-10 20:10:00 +0200

    Fix toggle handler

--- components/theme-toggle.tsx
+++ components/theme-toggle.tsx
@@ -1,24 +1,15 @@
 "use client";
 
-import { useEffect, useState } from "react";
 import { FaMoon, FaSun } from "react-icons/fa";
 
 const ThemeToggle = () => {
-  const [isDark, setIsDark] = useState(false);
-
-  useEffect(() => {
+  const handleToggle = () => {
     const isDark = document.documentElement.classList.contains("dark");
 
-    setIsDark(isDark);
-  }, []);
-
-  const handleToggle = () => {
     if (isDark) {
-      setIsDark(false);
       document.documentElement.classList.remove("dark");
       window.localStorage.setItem("theme", "light");
     } else {
-      setIsDark(true);
       document.documentElement.classList.add("dark");
       window.localStorage.setItem("theme", "dark");
     }
